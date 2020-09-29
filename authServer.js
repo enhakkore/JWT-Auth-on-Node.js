56908a48ae13c6eda65986f2425294e6a9881c67
enhakkore <ahn.ephraim@gmail.com>
2020-09-29 22:50:38 +0900

    feat: Add reissuing access token
    If the access token is expired, then check the refresh token.
    Then if the refresh token exists, reissue access token.

--- authServer.js
+++ authServer.js
@@ -5,6 +5,17 @@ const jwt = require('jsonwebtoken');
 
 app.use(express.json());
 
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  if(refreshToken === null) return res.sendStatus(401);
+  if(!refreshTokenStock.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, (error, user) => {
+    if(error) return res.sendStatus(403);
+    const accessToken = generateAccessToken({name: user.name});
+    res.json({ accessToken: accessToken, refreshToken: refreshToken})
+  })
+})
+
 app.post('/login', (req, res) => {
   // Authentication User
   
